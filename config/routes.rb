13724fe64b456490f81a8da0b81ef2504ed71f76
Carlo Garatti <carlo_garatti@yahoo.it>
2014-05-08 16:28:48 +0200

    add welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Ci::Application.routes.draw do
+  get "welcome/index"
   resources :games
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
